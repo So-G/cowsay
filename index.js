a4fbf4a29a07ccb0242910e764500b7f2e8c10b2
Solene <solene.gerard@gmail.com>
2021-03-18 23:39:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const myInfos = require('./information.js');
+const cowsay = require('cowsay');
+
+console.log(cowsay.say(myInfos));
---
+++ information.js
@@ -0,0 +1,8 @@
+let name = "Solene";
+let campus = "Lyon";
+
+const myInfos = {
+	text: `Hi I'm ${name} from ${campus}`,
+};
+
+module.exports = myInfos;
